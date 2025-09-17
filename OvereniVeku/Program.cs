ccdfa172f73f9e3984df3e85fb31aa650e9e2d70
Adam <adamhojer@seznam.cz>
2025-09-17 08:44:02 +0200

    OvereniVeku - novy projekt

---
+++ OvereniVeku/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string pokracovat;
+        do
+        {
+            Console.WriteLine("Zadej rok narození");
+
+            string text;
+
+            text = Console.ReadLine();
+
+            int cislo;
+
+            cislo = Int32.Parse(text);
+
+            if (cislo <= 2005)
+            {
+                Console.WriteLine("Plnoletý");
+            }
+            else
+            {
+                Console.WriteLine("Není plnoletý");
+            }
+            Console.WriteLine("Pokračovat?");
+            pokracovat = Console.ReadLine();
+        }
+        while (pokracovat == "ano");
+    }
+}
